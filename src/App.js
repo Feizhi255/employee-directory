a0878bb9c5e6cc2b278b9931618d43279ab74c8c
Jordan Kirby <jrock255@hotmail.com>
2020-12-07 12:21:13 -0700

    testing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+import bear from './img/bear2.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Jordan Kirby</h1>
+        <h3>Testing deploying to GitHub pages</h3>
+        <img src={bear} alt="bear" />
       </header>
     </div>
   );
